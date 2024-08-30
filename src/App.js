5bc78ff05a9e178a35876572a6948d7c1810c049
siddhu_from_sklm <codewithsiddhu@gmialo.com>
2024-08-30 23:54:32 +0530

    intial

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Tailwind CSS Example */}
+      <div className="text-center text-2xl text-blue-500">
+        Hello, Tailwind CSS!
+      </div>
+
+      {/* Bootstrap Example */}
+      <div className="text-center mt-4">
+        <button className="btn btn-primary">Hello, Bootstrap!</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
